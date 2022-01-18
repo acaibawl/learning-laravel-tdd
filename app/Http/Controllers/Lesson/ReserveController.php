c2a39140f19b2c0946f6049eb6089b06e1848c20
acaiibawl <hagiya0719@gmail.com>
2022-01-18 10:21:14 +0900

    テストダブルを用いたFeatureテスト

---
+++ app/Http/Controllers/Lesson/ReserveController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Lesson;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lesson;
+use App\Models\Reservation;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ReserveController extends Controller
+{
+    public function __invoke(Lesson $lesson)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        try {
+            $user->canReserve($lesson);
+        } catch (Exception $e) {
+            return back()->withErrors('予約できません。：' . $e->getMessage());
+        }
+        Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
+        
+        return redirect()->route('lessons.show', ['lesson' => $lesson]);
+    }
+}
